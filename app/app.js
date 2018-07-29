d5c42289206af47cb57bbae6c653a6420243f44f
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-07-30 01:29:23 +0200

    reverse modifs

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-var imageRouter = require('./routes/images')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+// var imageRouter = require('./routes/images')
 
 var app = express();
 
@@ -33,9 +33,9 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-app.use('/', imageRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// app.use('/', imageRouter);
 
 
 
