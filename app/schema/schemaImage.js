bb38327bc56a89985ce3527eac96c94f47ba95bd
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-08-06 23:07:07 +0200

    Added collection selection in schema

--- app/schema/schemaImage.js
+++ app/schema/schemaImage.js
@@ -5,7 +5,10 @@ var Schema = mongoose.Schema
 var ImageSchema = new Schema({
     imageSrc: String,
     description: String
-});
+    },{
+    collection:'Image'//Collection name in which to insert the data (if not specified mongoDB automatically creates a document to store the data)
+    }
+);
 
 var Image = mongoose.model('Image', ImageSchema);
 
